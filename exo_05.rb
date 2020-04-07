f45922979a400b8e472c154d41e029329208b2cc
Jeremy <jeremy44@protonmail.com>
2020-04-07 12:11:42 +0200

    exo_05

---
+++ exo_05.rb
@@ -0,0 +1,43 @@
+#{} execute le code le code contenu entre les accolades et convertit le resultat en chaine de caractères
+
+# affiche la chaine de caractères défini
+puts "On va compter le nombre d'heures de travail à THP"
+
+# calcule le contenu des accolades, convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Travail : #{10 * 5 * 11}"
+
+# calcule le contenu des accolades, convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+# affiche la chaine de caractères défini
+puts "Et en secondes ?"
+
+# calcule le produit, affiche le nombre obtenu
+puts 10 * 5 * 11 * 60 * 60
+
+# affiche la chaine de caractères défini (ni calcule ni comparaison ici)
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+# calcule, compare, affiche le resultat boolean
+puts 3 + 2 < 5 - 7
+
+# calcule le contenu des accolades, convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+
+# calcule le contenu des accolades, convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+# affiche la chaine de caractères défini
+puts "Ok, c'est faux alors !"
+
+# affiche la chaine de caractères défini
+puts "C'est drôle ça, faisons-en plus :"
+
+# calcule le contenu des accolades (comme c'est un test, le resultat est un boolean), convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+
+# calcule le contenu des accolades (comme c'est un test, le resultat est un boolean), convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+
+# calcule le contenu des accolades (comme c'est un test, le resultat est un boolean), convertit le resultat en chaines de caracteres, concatene les deux chaines, affiche le tout
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
