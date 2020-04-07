3dbf6d0ad9ae4c1dda126f86e498d0be18b1e9e9
Jeremy <jeremy44@protonmail.com>
2020-04-07 12:48:16 +0200

    exo_07

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# gets.champ est un equivalent de input en python : gets demande à l'utilisateur de saisir une chaine de caracteres finissant par un retour à la ligne ; gets.champ récupère le contenu saisi en enlevant le retour à la ligne
