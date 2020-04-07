1db09f5e50afafd83d28f2792ed549d25b97a47b
Jeremy <jeremy44@protonmail.com>
2020-04-07 12:56:12 +0200

    exo_09

---
+++ exo_09.rb
@@ -0,0 +1,9 @@
+puts "Quel est ton prénom ?"
+print "> "
+user_firstname = gets.chomp
+
+puts "Quel est ton nom de famille ?"
+print "> "
+user_name = gets.chomp
+
+puts "Bonjour, #{user_firstname} #{user_name} !"
