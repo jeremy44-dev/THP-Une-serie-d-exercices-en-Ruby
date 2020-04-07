8d9806b551f1e72e4c722c26541305d520f2cb7d
Jeremy <jeremy44@protonmail.com>
2020-04-07 10:28:21 +0200

    exo_02 - puts vs print

--- exo_02.rb
+++ exo_02.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 
 puts "Bonjour, monde !", "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# puts ajoute une ligne entre chaque argument s'il n'y en a pas deja une ; print non
