31568180b379449c6565fcac7b230cf7a814a540
Jeremy <jeremy44@protonmail.com>
2020-04-07 14:24:15 +0200

    exo_17

---
+++ exo_17.rb
@@ -0,0 +1,19 @@
+puts "Quelle est ton année de naissance ?"
+print "> "
+user_birth_year = gets.chomp.to_i
+current_year = Time.now.strftime("%Y").to_i
+
+if user_birth_year > current_year
+    puts "Menteur ! Pharisien !"
+else
+    user_birth_year.upto(current_year).each { |annee| 
+        delai = current_year - annee
+        age = annee - user_birth_year
+        
+        if delai == age
+            puts "Il y a #{delai} ans, tu avais la moitié de l'âge que tu as maujourd'hui"
+        else
+           puts "Il y a #{delai} ans, tu avais #{age} ans"
+        end
+    }    
+end
